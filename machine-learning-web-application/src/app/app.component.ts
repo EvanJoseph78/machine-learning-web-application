dbb31fef5323c4b035f3bb126667ee6bedd4384c
Evan Joseph <evandromariano49@gmail.com>
2024-01-09 11:49:41 -0300

    iniciado menu-bar

--- machine-learning-web-application/src/app/app.component.ts
+++ machine-learning-web-application/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenuBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'machine-learning-web-application';

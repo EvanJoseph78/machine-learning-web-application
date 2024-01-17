f68415ab978c9e27d4b943f5d21b4d702115f7ee
Evan Joseph <evandromariano49@gmail.com>
2024-01-17 01:00:01 -0300

    adcionado componetes: footer, course-page, main-page, adicionado back-end temporário

--- machine-learning-web-application/src/app/app.component.ts
+++ machine-learning-web-application/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuBarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MenuBarComponent,
+    MainPageComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

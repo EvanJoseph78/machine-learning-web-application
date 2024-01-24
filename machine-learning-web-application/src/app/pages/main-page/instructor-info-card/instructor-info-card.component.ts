85842ee7800c6d8c8530c95fc43afcc2af24d582
Evan Joseph <evandromariano49@gmail.com>
2024-01-24 03:47:49 -0300

    criado instructor-info-component

---
+++ machine-learning-web-application/src/app/pages/main-page/instructor-info-card/instructor-info-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructor-info-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './instructor-info-card.component.html',
+  styleUrl: './instructor-info-card.component.css'
+})
+export class InstructorInfoCardComponent {
+
+}
